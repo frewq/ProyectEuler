bcc84abbf6569329857810944c04d0593dc533ca
frewq <fabi85_592@msn.com>
2019-01-20 17:44:59 -0300

    nada

--- Problemas/Problema 9 - Special Pythagorean triplet/main.js
+++ Problemas/Problema 9 - Special Pythagorean triplet/main.js
@@ -1,7 +1,11 @@
-// A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+//projecteuler.net/problem=9
 
-// a2 + b2 = c2
-// For example, 32 + 42 = 9 + 16 = 25 = 52.
+https: // A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+// a^2 + b^2 = c^2
+// For example, 3^2 + 4^2 = 5^2 = 9 + 16 = 25.
 
 // There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 // Find the product abc.
+
+(1000 - c) ^ 2;
